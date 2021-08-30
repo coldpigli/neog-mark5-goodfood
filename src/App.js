c2afac64513cb36bec42eef000394ea05ef29e23
coldpigli <piyushdas98@gmail.com>
2021-08-30 22:32:43 +0530

    finished foodlab

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+
+const categories = ['all', ...new Set(items.map((item)=>item.category))] ; 
+
+
+const [foods, setFoods] = useState(items);
+
+const filterItem = (category) => {
+  if(category === 'all'){
+    setFoods(items);
+    return;
+  }
+  const newCategories=items.filter((item)=>item.category===category)
+  setFoods(newCategories);
+}
+
+  return <main >
+    <section className = "menu section">
+    <div className="title">
+      <h2>Piyush's Good Food List</h2>
+      <div className="underline"></div>
+
+    </div>
+    <Categories categories={categories} filterItem = {filterItem}/>
+    <Menu foods = {foods}/>
+    </section>
+    
+    
+  </main>;
 }
 
 export default App;
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({foods}) => {
+    return <article className = "section-center">
+      {
+        foods.map((food)=>{
+          const {id, title, price, img, desc} = food;
+          return <div key={id} className = "menu-item">
+            <img src={img} alt = {title} className = "photo"></img>
+            <div className = "item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className = "price">${price}</h4>
+              </header>
+              <h3 className = "item-text">{desc}</h3>
+            </div>
+          </div>
+        })
+      }
+    </article>
 };
 
-export default Menu;
+export default Menu
